1b37711d6664efbf0a77239610a2e1554e67b994
Igor Malyarov <photo.igor@mac.com>
2019-08-14 11:30:43 +0300

    Initial Commit

---
+++ ExtensionsBarsAndCards/ExtensionsBarsAndCards.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionsBarsAndCards.h
+//  ExtensionsBarsAndCards
+//
+//  Created by Igor Malyarov on 14.08.2019.
+//  Copyright © 2019 Igor Malyarov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExtensionsBarsAndCards.
+FOUNDATION_EXPORT double ExtensionsBarsAndCardsVersionNumber;
+
+//! Project version string for ExtensionsBarsAndCards.
+FOUNDATION_EXPORT const unsigned char ExtensionsBarsAndCardsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionsBarsAndCards/PublicHeader.h>
+
+
